4bc636b0559865d51fef719d245466099eabfd48
Leon <leonamorim.l93@gmail.com>
2023-08-23 17:00:30 -0300

    refatorado

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let tasks = [];
 
-
-const createCard = (taskInfo, index) => {
+const createCard = ({ title, type }, index) => {
   // Criando elementos necessários
   const taskCardItem = document.createElement("li");
   const taskCardContent = document.createElement("div");
@@ -9,29 +8,30 @@ const createCard = (taskInfo, index) => {
   const taskDescription = document.createElement("p");
 
   // Adicionando o titulo da tarefa como texto do paragrafo
-  taskDescription.innerText = taskInfo.title;
+  taskDescription.innerText = title;
 
   // Adicionando span e paragrafo a div
   taskCardContent.appendChild(taskTitle);
   taskCardContent.appendChild(taskDescription);
+
   // taskCardContent.appendChild(buttonTrash);
   //Adicionando classe ao span
 
-  if (taskInfo.type == "Urgente") {
+  if (type == "Urgente") {
     taskTitle.classList.add("span-urgent");
-  } else if (taskInfo.type == "Prioritário") {
+  } else if (type == "Prioritário") {
     taskTitle.classList.add("span-priority");
   } else {
     taskTitle.classList.add("span-normal");
   }
 
-   // Criando botão para deletar tarefa
-   const buttonDelete = document.createElement("button");
-  
-   // adicionando id
-   buttonDelete.classList.add('deleteBtt')
-   buttonDelete.id = index
-   // Adicionando icone ao botão
+  // Criando botão para deletar tarefa
+  const buttonDelete = document.createElement("button");
+
+  // adicionando id
+  buttonDelete.classList.add("deleteBtt");
+  buttonDelete.id = index;
+  // Adicionando icone ao botão
   buttonDelete.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
 
   /// Adicionando a div e o botão de deletar ao list item
@@ -39,82 +39,72 @@ const createCard = (taskInfo, index) => {
   taskCardItem.appendChild(buttonDelete);
 
   return taskCardItem;
-}
+};
 
 const renderTask = (taskList) => {
-  const htmlList = document.querySelector(".tasks");
-  htmlList.innerHTML = "";
+  const tasksContainer = document.querySelector(".tasks__container");
+  tasksContainer.innerHTML = "";
 
-  // Ajustar a lógica   
-  
-  for (let i = 0; i < taskList.length; i++) {
-    
-    const card = createCard(taskList[i],i);
-    htmlList.appendChild(card);
-  }
+  // Ajustar a lógica
+  taskList.forEach((task, i) => {
+    const card = createCard(task, i);
+    tasksContainer.appendChild(card);
+  });
+};
 
-
-}
-
-const addTask = ()=>{
+const addTask = () => {
   const button = document.querySelector("#btnSubmit");
 
-  button.addEventListener("click", function (e) {
+  button.addEventListener("click", (e) => {
     e.preventDefault();
-  
+
     // Pegandoo a Tarefa
     let titulo = document.getElementById("input_title");
-  
+
     let valueTitle = titulo.value;
-    
+
     // PEGANDO PRIORIDADE
     let prioridade = document.querySelector("#input_priority");
     let valuePriority = prioridade.value;
-  
+
     const push = tasks.push({ title: valueTitle, type: valuePriority });
 
-    localStorage.setItem('tasksList', JSON.stringify(tasks))
-    
+    localStorage.setItem("tasksList", JSON.stringify(tasks));
+
     renderTask(tasks);
-    removeTask()
+    removeTask();
+
     titulo.value = "";
-    return push
+
+    return push;
   });
-}
+};
 
-const removeTask = ()=>{
-
-  const deleteButton = document.querySelectorAll(".deleteBtt")
+const removeTask = () => {
+  const deleteButton = document.querySelectorAll(".deleteBtt");
   //criando evento botão deletar
 
-  deleteButton.forEach((btt)=>{
+  deleteButton.forEach((btt, index) => {
+    btt.addEventListener("click", (e) => {
+      tasks.splice(index, 1);
 
-    btt.addEventListener("click",(e)=> {
-      
-      console.log(e)
-      let index = 0;
-      for (let i = 0; i < tasks.length; i++) {
-        tasks.splice(index, 1);
+      renderTask(tasks);
 
-        console.log(tasks)
-        renderTask(tasks);
+      localStorage.setItem("tasksList", JSON.stringify(tasks));
+
+      if (tasks.length === 0) {
+        localStorage.clear();
       }
-      localStorage.setItem('tasksList', JSON.stringify(tasks))
-
-      if(tasks.length === 0){
-        localStorage.clear()
-      }
-      removeTask(tasks)
+      removeTask(tasks);
     });
-  })
+  });
+};
 
-}
-
-const filterTask = ()=>{
-
+const filterTask = () => {
   // chamando o input de pesquisa
-  
+
   const inputPesquisa = document.querySelector("#pesquisa");
+  inputPesquisa.value = "";
   // colocando um evento no input
   inputPesquisa.addEventListener("input", function (e) {
     let textoInput = e.target.value.toLowerCase();
@@ -123,32 +113,28 @@ const filterTask = ()=>{
     const meuNovoArray = tasks.filter((itens) => {
       //verificadno se o que tem no array é igual ao que está no input
       let textPesquisado = itens.title.toLowerCase().includes(textoInput);
-      
-      console.log(itens)
+
+      console.log(itens);
       return textPesquisado;
     });
     // passando o novo array como parametro.
     renderTask(meuNovoArray);
   });
-}
+};
 
-const readLocalStorage = ()=>{
-  const getArray = localStorage.getItem('tasksList')
+const readLocalStorage = () => {
+  const getArray = localStorage.getItem("tasksList");
 
-  if(getArray){
+  if (getArray) {
     const arrayParse = JSON.parse(getArray);
 
-    arrayParse.forEach(task => 
-      tasks.push(task))
+    arrayParse.forEach((task) => tasks.push(task));
 
-    renderTask(tasks)
-
+    renderTask(tasks);
   }
-  
-}
+};
 
-
-addTask()
-readLocalStorage()
-removeTask()
-filterTask()  
+addTask();
+readLocalStorage();
+removeTask();
+filterTask();
